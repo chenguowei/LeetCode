f88d1e1ef5adc3c5d862def864304e5b6955535d
chenguowei <457219884@qq.com>
2018-03-19 13:41:21 +0800

    reverse Integer

--- 001. Two Sum/solution.h
+++ 001. Two Sum/solution.h
@@ -1,9 +1,35 @@
 //
 // Created by 陈国威 on 2018/3/13.
 //
+//Given nums = [2, 7, 11, 15], target = 9,
+
+//Because nums[0] + nums[1] = 2 + 7 = 9,
+//return [0, 1].
 
 #ifndef LEETCODE_SOLUTION_H
 #define LEETCODE_SOLUTION_H
 
+#include <vector>
+using std::vector;
+#include <unordered_map>
+using std::unordered_map;
 
+class Solution
+{
+ public:
+  vector<int> twoSum(vector<int>& nums, int target)
+  {
+	std::unordered_map<int, int> record;
+
+	for (int i = 0; i != nums.size (); ++i)
+	  {
+		auto found = record.find (nums[i]);
+		if ( found != record.end ())
+		  return {found->second, i};
+
+		record.emplace (target - nums[i], i);
+
+	  }
+  }
+};
 #endif //LEETCODE_SOLUTION_H
